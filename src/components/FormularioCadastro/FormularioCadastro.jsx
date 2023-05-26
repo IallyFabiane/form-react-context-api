029938a2973c72feb2427198a4e0ce315e1ca70e
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-05-26 12:39:35 -0300

    useEffect e Stepper do material ui

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,22 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosEntrega from "./DadosEntrega";
 import DadosUsuario from "./DadosUsuario";
+import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
 
 function FormularioCadastro({aoEnviar, validarCPF}) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const formularios = [
-  <DadosUsuario aoEnviar={proximo} />, 
-  <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
-  <DadosEntrega aoEnviar={aoEnviar} />
+  <DadosUsuario aoEnviar={coletarDados} />, 
+  <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+  <DadosEntrega aoEnviar={coletarDados} />,
+  <Typography variant="h5">Obrigado pelo cadastro!</Typography>
   ];
+  const [dadosColetados, setDados] = useState({});
+  
+  useEffect(() => {
+    if(etapaAtual === formularios.length - 1) {
+      aoEnviar(dadosColetados);
+    }
+  })
 
-  function proximo (dados) {
+  function coletarDados(dados) {
+    setDados({...dadosColetados, ...dados});
+    proximo();
+  }
+
+  function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
   
   return (
     <>
+      <Stepper activeStep={etapaAtual}>
+        <Step><StepLabel>Login</StepLabel></Step>
+        <Step><StepLabel>Pessoal</StepLabel></Step>
+        <Step><StepLabel>Entrega</StepLabel></Step>
+        <Step><StepLabel>Finalização</StepLabel></Step>
+      </Stepper>
       {formularios[etapaAtual]}
     </>
   )
