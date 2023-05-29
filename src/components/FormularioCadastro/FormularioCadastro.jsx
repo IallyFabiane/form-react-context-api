a940ac8b89344f6186f4f87a10699805103b99b2
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-05-29 15:37:43 -0300

    utilizando contextos

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -4,12 +4,12 @@ import DadosEntrega from "./DadosEntrega";
 import DadosUsuario from "./DadosUsuario";
 import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
 
-function FormularioCadastro({aoEnviar, validacoes}) {
+function FormularioCadastro({aoEnviar}) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const formularios = [
-  <DadosUsuario aoEnviar={coletarDados} validacoes={validacoes} />, 
-  <DadosPessoais aoEnviar={coletarDados} validacoes={validacoes} />,
-  <DadosEntrega aoEnviar={coletarDados} validacoes={validacoes} />,
+  <DadosUsuario aoEnviar={coletarDados}  />, 
+  <DadosPessoais aoEnviar={coletarDados} />,
+  <DadosEntrega aoEnviar={coletarDados}/>,
   <Typography variant="h5">Obrigado pelo cadastro!</Typography>
   ];
   const [dadosColetados, setDados] = useState({});
