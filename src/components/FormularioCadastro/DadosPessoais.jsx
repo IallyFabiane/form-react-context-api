9ecf72470569a23e3bef15292dc69dede2bbaaba
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-05-29 12:56:24 -0300

    criando models para validações

--- src/components/FormularioCadastro/DadosPessoais.jsx
+++ src/components/FormularioCadastro/DadosPessoais.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
-function DadosPessoais({aoEnviar, validarCPF}) {
+function DadosPessoais({aoEnviar, validacoes}) {
   const [nome, setNome] = useState("");
   const [sobrenome, setSobrenome] = useState("");
   const [cpf, setCpf] = useState("");
   const [promocoes, setPromocoes] = useState(true);
   const [novidades, setNovidades] = useState(false);
-  const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
+  const [erros, setErros] = useState({cpf:{valido:true, texto:""}});
+
+  function validarCampos(event) {
+      const {name, value} = event.target;
+      const novoEstado = {...erros}
+      const ehValido = validacoes[name](value);
+      novoEstado[name] = ehValido;
+      setErros(novoEstado);
+  }
+
   return (
     <form
       onSubmit={(event) => {
@@ -43,16 +52,14 @@ function DadosPessoais({aoEnviar, validarCPF}) {
           setCpf(event.target.value);
         }}
 
-        onBlur={(event)=>{
-          const ehValido = validarCPF(cpf);
-          setErros({cpf:ehValido})
-        }}
+        onBlur={validarCampos}
         error={!erros.cpf.valido}
         helperText={erros.cpf.texto}
         id="CPF"
         label="CPF"
         variant="outlined"
         margin="normal"
+        name="cpf"
         fullWidth
       />
 
